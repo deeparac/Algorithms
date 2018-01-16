67bf717cf0db619f79e677ea6229cda92cb21361
deeparac <deeparaclearning@gmail.com>
2018-01-15 22:55:17 -0800

    matching

---
+++ Distinct Subsequence.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    /*
+     * @param : A string
+     * @param : A string
+     * @return: Count the number of distinct subsequences
+     */
+    int numDistinct(string S, string T) {
+        // write your code here
+        int m = S.size();
+        int n = T.size();
+
+        vector<vector<int>> dp(m+1, vector<int>(n, 0));
+        for (int i = 0; i <= m ;++i) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                dp[i][j] = dp[i-1][j];
+                if (S[i-1] == T[j-1]) {
+                    dp[i][j] += dp[i-1][j-1];
+                }
+            }
+        }
+
+        // for (int i = 0; i <= m; ++i) {
+        //     for (int j = 0; j <= n; ++j) {
+        //         cout << dp[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        return dp[m][n];
+    }
+};
